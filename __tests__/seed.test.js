59afdc2cc7893562f0a8004f8fa82c1881b02e5e
Kasha Malloy <kashamalloy@gmail.com>
2023-11-20 18:45:05 +0000

    working on task 3

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -14,7 +14,7 @@ afterAll(() => db.end());
 
 
 describe('GET /api/topics', () => {
-    test('GET:200 sends a 200 status code', () => {
+    test('GET:200 sends a 200 status code and array of topics to the client', () => {
         return request(app)
             .get('/api/topics')
             .expect(200).then((res) => {
@@ -31,12 +31,22 @@ describe('GET /api/topics', () => {
                 })
             })
     });
-    test('GET:400 returns an err msg if api path is invalid', () => {
+});
+
+describe('GET:404 - bad path', () => {
+    test('GET:404 returns status code of 404 and "path not found" msg if api path is invalid', () => {
         return request(app).get('/api/topiks')
             .expect(404).then((res) => {
                 expect(res.body.msg).toBe('path not found');
             })
     })
-});
+})
 
-
+describe.only('GET /api', () => {
+    test("GET:200 returns status code of 200, object containing all apis", () => {
+        return request(app).get('/api')
+            .expect(500).then((res) => {
+                console.log(res.body)
+            })
+    })
+})
